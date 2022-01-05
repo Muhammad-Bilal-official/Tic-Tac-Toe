c096224792e75b5f86f09d8c62425a4587cd42dc
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-01-05 23:36:10 +0000

    Tic Tak Toe improved

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
 console.log("Tic Tac Toe Game");
 let turn = "X";
 let turnCount = 0;
+let turnContainer = document.getElementById("turnContainer");
+let turnBtnX = document.getElementById("turnBtnX");
+let turnBtn0 = document.getElementById("turnBtn0");
 let winAnnouncement = document.getElementById("winAnnouncement");
+let gameInfoHeading = document.getElementById("gameInfoHeading");
 
 function changeTurn() {
     turn = (turn === "X") ? "0" : "X";
     turnCount++;
+    if(turn==="X"){
+        console.log("X turn")
+        turnBtnX.style.border = "4px solid red";
+        turnBtn0.style.border = "2px solid black";
+    }
+    else{
+        console.log("0 turn")
+        turnBtn0.style.border = "4px solid red";
+        turnBtnX.style.border = "2px solid black";
+    }
+
 }
 let chembers = document.getElementsByClassName("chembers");
 Array.from(chembers).forEach((chember) => {
@@ -28,11 +43,17 @@ Array.from(chembers).forEach((chember) => {
                 console.log(winText);
                 winAnnouncement.innerHTML = winText;
                 resetGameBtn.style.display = "block";
+                turnContainer.style.display = "none";
+                gameInfoHeading.style.display = "none";
+
                 // resetGame();
             }
         }
         if (turnCount === 9) {
             winAnnouncement.innerHTML = "Match Drawn";
+            resetGameBtn.style.display = "block";
+            turnContainer.style.display = "none";
+            gameInfoHeading.style.display = "none";
             turnCount = 0;
         }
     });
@@ -68,4 +89,9 @@ function resetGame() {
     winText = undefined;
     winAnnouncement.innerHTML = "";
     resetGameBtn.style.display = "none";
+    
+    turnContainer.style.display = "block";
+    turnBtnX.style.border = "4px solid red";
+    turnBtn0.style.border = "2px solid black";
+    gameInfoHeading.style.display = "block";
 }
